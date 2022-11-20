d9611c2a1aaf12a33ddaba101f27c03567986670
lagou_zimu <lagou_zimu@lagou.com>
2022-11-20 22:05:43 +0800

    删除git测试的内容

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -49,22 +49,6 @@ public interface UserMapper {
      */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
 
-    public List<Resource> findResourceByRoleId2(List<Integer> ids);
 
-    public void test11();
-    public void test21();
-    public void test31();
-    public void test41();
-    public void test51();
-    public void test61();
-    public void test71();
-
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
 
 }
