5e42d8877097febcf18fd5d7e03bea3547c5d118
lagou_zimu <lagou_zimu@lagou.com>
2022-11-20 21:51:28 +0800

    update UserMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -51,4 +51,12 @@ public interface UserMapper {
 
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
 
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
+
 }
